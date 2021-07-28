31464f04b3b9c145bea2d0d527f59aa35a54b67a
thiagobergami <thiagobergamig@gmail.com>
2021-07-28 15:27:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Bloco responsável por chamar os dados da API e escrever na variavel data
+
+let data = '';
+async function readApi(){
+    let url = 'https://safe-sierra-45694.herokuapp.com/user';
+    try{
+        let response = await fetch(url);
+        data = await response.json();
+    }catch(err){
+        console.log(err);
+    }
+}
+//Após executar a função, a variavel data receberá o objeto vindo da API.
+//Se quiser verificar o modelo, acesse o arquivo data-sample.json
+readApi();
+
+function main(){
+    //Chame todas as funções dentro da função main;
+}
